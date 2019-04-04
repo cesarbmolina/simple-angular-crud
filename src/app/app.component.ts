7a24ab2c88526af583016486ac4f426cec1fa05f
Cesar Molina <cesar@multiplica.com>
2019-04-04 13:54:52 -0500

    Feat(app): CRUD simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+import { EmptyOutletComponent } from '@angular/router/src/components/empty_outlet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-crud';
+
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Ryan', country: 'USA' },
+    { id: 2, name: 'John', country: 'Australia' },
+    { id: 3, name: 'Abdul', country: 'India' }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+
+  }
+
+  delete() {
+    if (confirm('Are you sure delete?')) {
+      this.employeeArray = this.employeeArray.filter(x => x !== this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+
+  }
+
 }
